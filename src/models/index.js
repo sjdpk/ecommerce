bc3e0d8253f8723b422ac80a0f996747e6664a99
Deepak Sapkota <sapkotad64@gmail.com>
2022-11-23 07:54:16 +0545

    associations added

--- src/models/index.js
+++ src/models/index.js
@@ -24,14 +24,27 @@ db.category = require('./category.model')(sequelize,DataTypes,'category');
 db.subcategory = require('./category.model')(sequelize,DataTypes,'subcategory');
 db.subSubcategory = require('./category.model')(sequelize,DataTypes,'subsubcategory');
 
+db.department = require('./department.model')(sequelize,DataTypes);
+
 db.product = require('./product.model')(sequelize,DataTypes);
+db.cart = require('./cart.model')(sequelize,DataTypes);
+db.coupon = require('./coupon.model')(sequelize,DataTypes);
+db.order = require('./order.model')(sequelize,DataTypes);
+
+db.user = require('./user.model')(sequelize,DataTypes);
 
 
 // Associations
 Category = db.category;
 SubCategory = db.subcategory; 
 SubSubCategory = db.subSubcategory;
+Department = db.department;
 Product = db.product;
+Cart = db.cart;
+Coupon = db.coupon;
+Order = db.order;
+User = db.user;
+
 
 /* 1.one category have many sub category but 
     one subcategory belongs to only one category
@@ -78,6 +91,51 @@ SubSubCategory.hasMany(Product,{
     as:'subsubcategory',
 });
 Product.belongsTo(SubSubCategory);
+
+/*
+  * product and dpartment relation
+  * one department contains many product
+  * many product belongs to one department
+  * relaton : [ department hasMany product ]
+  * relation : [ product belongsTo Department ]
+*/
+Department.hasMany(Product,{
+    foreignKey: 'departmentId',
+    as:'department',
+});
+Product.belongsTo(Department);
+
+
+/* 
+    *department and user relationship
+    *user has many department
+    *many department belongs to one user
+    *relation [ user hasmany department]
+    *relation [department belongs to user]
+*/
+User.hasMany(Department,{
+    foreignKey:'departmentHeadId',
+    as:'user',
+});
+Department.belongsTo(User);
+
+/*
+    * product and coupon relationship
+    * one coupon have many product
+    * one product have many coupon
+    * relation [ Many-To-Many ]
+*/
+Coupon.belongsToMany(Product, { through: 'coupon_product' });
+Product.belongsToMany(Coupon, { through: 'coupon_product' });
+
+/*
+    * category and coupon relationship
+    * one coupon have many category
+    * one category have many coupon
+    * relation [ Many-To-Many ]
+*/
+Coupon.belongsToMany(Category, { through: 'coupon_category' });
+Category.belongsToMany(Coupon, { through: 'coupon_category' });
 
 
 // sysncing database
